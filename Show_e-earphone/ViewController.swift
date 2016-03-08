890d92faf34d43aa1622d530dd7cdc59024c5b58
nitaking <johin@example.com>
2016-03-09 08:22:30 +0900

    Initial Commit

---
+++ Show_e-earphone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Show_e-earphone
+//
+//  Created by Satoshi Nitawaki on 2016/03/09.
+//  Copyright © 2016年 仁田脇 理史. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
